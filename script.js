8b40a4604b8ee6df4442ee74f5a7f9ea41b0bb45
shreshtha21 <shreshthagupta2111@gmail.com>
2025-12-10 08:21:00 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = {
+    threshold: 0.2, 
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
