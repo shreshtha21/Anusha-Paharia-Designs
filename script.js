26027eb9e32f998db1fa85c558855a3bb9d77c01
Shreshtha Gupta <shreshthagupta2111@gmail.com>
2025-12-10 14:35:08 +0530

    theme switcher for dark mode
    Added theme switching functionality to toggle dark mode.

--- script.js
+++ script.js
@@ -28,3 +28,10 @@ const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+function switchTheme(e) {
+    if (e.target.checked) document.body.classList.add('dark-mode');
+    else document.body.classList.remove('dark-mode');
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
